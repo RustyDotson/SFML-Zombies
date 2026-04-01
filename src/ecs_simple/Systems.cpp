7c913711b96fddebf7f1fee436271490b073057e
Russ <32114457+RustyDotson@users.noreply.github.com>
2026-04-01 12:00:28 +0900

    correct bullet speed no matter which direction character is moving.

--- src/ecs_simple/Systems.cpp
+++ src/ecs_simple/Systems.cpp
@@ -211,8 +211,8 @@ void ShootingSystem::update(Registry& reg, Game& game, float dt) {
                     playerTags[e].timeSinceLastShot += dt;
                     if (playerTags[e].timeSinceLastShot >= playerTags[e].fireRate) {
                         playerTags[e].timeSinceLastShot = 0.f; // Reset the timer
-                        float velocity_x = cos(transforms[e].rotation.asRadians()) * 500.f; // Adjust bullet speed
-                        float velocity_y = sin(transforms[e].rotation.asRadians()) * 500.f;
+                        float velocity_x = (cos(transforms[e].rotation.asRadians()) * 500.f) + transforms[e].velocity_x; // Adjust bullet speed
+                        float velocity_y = (sin(transforms[e].rotation.asRadians()) * 500.f) + transforms[e].velocity_y; // Inherit player's vertical velocity
                         game.createBullet(transforms[e].rotation, velocity_x, velocity_y, transforms[e].position);
                         //spawnSystem.createBullet(reg, transforms[e].rotation, velocity_x, velocity_y, transforms[e].position);
                     } 
@@ -307,13 +307,14 @@ void SpawnSystem::manageAsteroids(Registry& reg, sf::RenderWindow& window, Game&
         sf::Vector2u window_size = window.getSize();
         sf::Vector2f spawn_coords = utils::rand_bord_coord(window_size);
         sf::Vector2f direction_to_center = sf::Vector2f(window_size.x/2, window_size.y/2) - spawn_coords;
+
         float angle_to_center = atan2(direction_to_center.y, direction_to_center.x);
         float angle_degrees = angle_to_center * 180 / 3.14159f; // Convert to degrees
         float random_offset = utils::rand_float(-30.f, 30.f); // Random offset in degrees
         angle_degrees += random_offset; // Add random offset to the angle
         angle_to_center = angle_degrees * 3.14159f / 180; // Convert back to radians
 
-        float speed = 100.f; // Adjust this value for faster/slower asteroids
+        float speed = 100.f;
         float vx = cos(angle_to_center) * speed;
         float vy = sin(angle_to_center) * speed;
         
